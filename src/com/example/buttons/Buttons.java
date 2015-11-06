332601fb937573520d93844e518929fd61b5ef20
cjbaskin123 <cjbaskin123@gmail.com>
2015-11-05 23:58:04 -0800

    Initial Commit

---
+++ src/com/example/buttons/Buttons.java
@@ -0,0 +1,55 @@
+package com.example.buttons;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.content.Intent;
+
+public class Buttons extends Activity {
+
+    public static final String EXTRA_MESSAGE = "com.example.Buttons.MESSAGE";
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.buttons, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        Log.i("tag", String.format("%d", id));
+        if (id == R.id.action_settings) {
+            return true;
+           
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void harderschnoff (View view) {
+    ImageButton ibutton = (ImageButton) findViewById(R.id.imageButton1);
+    ibutton.setOnClickListener (new View.OnClickListener()
+    {
+        public void onClick(View v)
+        {
+            setContentView(R.layout.activity_newpage);
+        }
+    });
+   
+}
+}
